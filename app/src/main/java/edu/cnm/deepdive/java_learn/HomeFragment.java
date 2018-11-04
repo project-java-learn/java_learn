408578562d46dbc2435037ec70b19461b304cd9f
Matthew Morgan <mmorgan55@cnm.edu>
2018-11-04 11:24:54 -0700

    Made HomeFragment default landing upon opening app and added logic for navigation to LevelsFragment, ProfileFragment, and GlossaryFragment.

--- app/src/main/java/edu/cnm/deepdive/java_learn/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/java_learn/HomeFragment.java
@@ -5,25 +5,62 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HomeFragment extends Fragment {
 
-
-  public HomeFragment() {
-    // Required empty public constructor
-  }
-
+  private Button levelsButton;
+  private Button profileButton;
+  private Button glossaryButton;
+  private Button settingsButton;
+  private OnClickListener listener;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
+
+    View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+    levelsButton = view.findViewById(R.id.levels_button);
+    profileButton = view.findViewById(R.id.profile_button);
+    glossaryButton = view.findViewById(R.id.glossary_button);
+    settingsButton = view.findViewById(R.id.settings_button);
+
     return inflater.inflate(R.layout.fragment_home, container, false);
+  }
+
+
+  private void setupListener() {
+    listener = new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        int id = v.getId();
+        Fragment fragment;
+
+        switch (id) {
+          case R.id.levels_button:
+            fragment = new LevelsFragment();
+            break;
+          case R.id.profile_button:
+            fragment = new ProfileFragment();
+            break;
+          case R.id.glossary_button:
+            fragment = new GlossaryFragment();
+            break;
+          case R.id.settings_button:
+            fragment = new GlossaryFragment();
+            break;
+          default:
+            fragment = new LevelsFragment();
+        }
+
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment)
+            .addToBackStack("home").commit();
+      }
+    };
   }
 
 }
