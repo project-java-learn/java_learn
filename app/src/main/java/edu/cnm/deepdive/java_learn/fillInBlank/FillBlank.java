9a63de869441748907ea51830e0d7fa7e38be835
creys <creysmith@gmail.com>
2018-11-14 14:03:46 -0700

    add xml changes to fill in the blank

--- app/src/main/java/edu/cnm/deepdive/java_learn/fillInBlank/FillBlank.java
+++ app/src/main/java/edu/cnm/deepdive/java_learn/fillInBlank/FillBlank.java
@@ -1,18 +1,24 @@
-package edu.cnm.deepdive.java_learn;
+package edu.cnm.deepdive.java_learn.fillInBlank;
+
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import edu.cnm.deepdive.java_learn.R;
 
 
-public class HighlightFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FillBlank extends Fragment {
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_highlight, container, false);
+    return inflater.inflate(R.layout.fragment_fill_blank, container, false);
   }
+
 }
