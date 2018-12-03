62a507c25d1fa6854af866abd8e7ae76d047c2f2
creys <creysmith@gmail.com>
2018-12-03 09:58:49 -0700

    added some functionality for login and signup. added xml for signup and modified xml for login

--- app/src/main/java/edu/cnm/deepdive/java_learn/signupActivity.java
+++ app/src/main/java/edu/cnm/deepdive/java_learn/signupActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,107 +13,97 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class login extends AppCompatActivity {
-  private static final String TAG = "LoginActivity";
-  private static final int REQUEST_SIGNUP = 0;
+public class signupActivity extends AppCompatActivity {
+  private static final String TAG = "SignupActivity";
 
+  @BindView(R.id.input_name) EditText _nameText;
   @BindView(R.id.email_input) EditText _emailText;
   @BindView(R.id.password_input) EditText _passwordText;
-  @BindView(R.id.login_button) Button _loginButton;
-  @BindView(R.id.link_signup) TextView _signupLink;
+  @BindView(R.id.signup_button) Button _signupButton;
+  @BindView(R.id.link_login) TextView _loginLink;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.fragment_login);
+    setContentView(R.layout.fragment_signup);
     ButterKnife.bind(this);
 
-    _loginButton.setOnClickListener(new View.OnClickListener() {
-
+    _signupButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        login();
+        signup();
       }
     });
 
-    _signupLink.setOnClickListener(new View.OnClickListener() {
-
+    _loginLink.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // Start the Signup activity
-        Intent intent = new Intent(getApplicationContext(), signupActivity.class);
-        startActivityForResult(intent, REQUEST_SIGNUP);
+        // Finish the registration screen and return to the Login activity
+        finish();
       }
     });
   }
 
-  public void login() {
-    Log.d(TAG, "Login");
+  public void signup() {
+    Log.d(TAG, "Signup");
 
     if (!validate()) {
-      onLoginFailed();
+      onSignupFailed();
       return;
     }
 
-    _loginButton.setEnabled(false);
+    _signupButton.setEnabled(false);
 
-    final ProgressDialog progressDialog = new ProgressDialog(login.this,
+    final ProgressDialog progressDialog = new ProgressDialog(signupActivity.this,
         R.style.AppTheme_Dark);
     progressDialog.setIndeterminate(true);
-    progressDialog.setMessage("Authenticating...");
+    progressDialog.setMessage("Creating Account...");
     progressDialog.show();
 
+    String name = _nameText.getText().toString();
     String email = _emailText.getText().toString();
     String password = _passwordText.getText().toString();
 
-    // TODO: Implement your own authentication logic here.
+    // TODO: Implement your own signup logic here.
 
     new android.os.Handler().postDelayed(
         new Runnable() {
           public void run() {
-            // On complete call either onLoginSuccess or onLoginFailed
-            onLoginSuccess();
-            // onLoginFailed();
+            // On complete call either onSignupSuccess or onSignupFailed
+            // depending on success
+            onSignupSuccess();
+            // onSignupFailed();
             progressDialog.dismiss();
           }
         }, 3000);
   }
 
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == REQUEST_SIGNUP) {
-      if (resultCode == RESULT_OK) {
-
-        // TODO: Implement successful signup logic here
-        // By default we just finish the Activity and log them in automatically
-        this.finish();
-      }
-    }
-  }
-
-  @Override
-  public void onBackPressed() {
-    // disable going back to the MainActivity
-    moveTaskToBack(true);
-  }
-
-  public void onLoginSuccess() {
-    _loginButton.setEnabled(true);
+  public void onSignupSuccess() {
+    _signupButton.setEnabled(true);
+    setResult(RESULT_OK, null);
     finish();
   }
 
-  public void onLoginFailed() {
+  public void onSignupFailed() {
     Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-    _loginButton.setEnabled(true);
+    _signupButton.setEnabled(true);
   }
 
   public boolean validate() {
     boolean valid = true;
 
+    String name = _nameText.getText().toString();
     String email = _emailText.getText().toString();
     String password = _passwordText.getText().toString();
+
+    if (name.isEmpty() || name.length() < 3) {
+      _nameText.setError("at least 3 characters");
+      valid = false;
+    } else {
+      _nameText.setError(null);
+    }
 
     if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
       _emailText.setError("enter a valid email address");
