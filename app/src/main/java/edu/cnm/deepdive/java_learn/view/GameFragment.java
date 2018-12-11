93b697c83fc127908372a2e75465ad58aad93cbd
Matthew Morgan <mmorgan55@cnm.edu>
2018-12-11 09:38:42 -0700

    Added logic for updating progress to GameFragment.

--- app/src/main/java/edu/cnm/deepdive/java_learn/view/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/java_learn/view/GameFragment.java
@@ -1,8 +1,69 @@
 package edu.cnm.deepdive.java_learn.view;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
+import edu.cnm.deepdive.java_learn.R;
+import edu.cnm.deepdive.java_learn.model.pojo.ProgressPojo;
+import edu.cnm.deepdive.java_learn.service.JavaLearnApplication;
+import edu.cnm.deepdive.java_learn.service.JavaLearnService;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public abstract class GameFragment extends Fragment {
 
+  private Retrofit retrofit;
+  private JavaLearnService service;
+
+  protected void updateProgress() {
+    new UpdateProgressTask().execute();
+  }
+
+  private void setupRetrofit() {
+    retrofit = new Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:28082/rest/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+
+    service = retrofit.create(JavaLearnService.class);
+  }
+
+  private class UpdateProgressTask extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected void onPreExecute() {
+      setupRetrofit();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+      ProgressPojo pojo;
+      String token = getString(R.string.authorization_header_format,
+          JavaLearnApplication.getInstance().getAccount().getIdToken());
+      try {
+        Response<ProgressPojo> response = service.checkProgress(token).execute();
+        if (response.isSuccessful()) {
+          pojo = response.body();
+          List<String> moreLevels = pojo.getLevels();
+          int score = pojo.getScore();
+          moreLevels.add("check check");
+          score += 250;
+
+          pojo.setLevels(moreLevels);
+          pojo.setScore(score);
+
+          service.updateProgress(token, pojo).execute();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      return null;
+    }
+  }
 
 }
